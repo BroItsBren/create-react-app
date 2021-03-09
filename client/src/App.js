d24f30ab340297d4af8f401732c911035bf22c1e
BroItsBren <jzr5192@autuni.ac.nz>
2021-03-10 03:58:08 +1300

    Client app.js edit

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   constructor(props) {
@@ -14,26 +14,24 @@ callAPI()
         .then(res => res.text())
         .then(res => this.setState({ apiResponse: res }));
 }
-componentWillMount() 
+
+componentDidMount() 
 {
     this.callAPI();
 }
 
 render() {
+   return (
+     <div className="App">
+       <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+         <h1 ClassName="App-intro">Welcome to React</h1>
+         </header>
+          <p className="App-intro">{this.state.apiResponse}</p>
+      </div>
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 ClassName="App-intro">Welcome to React</h1>
-        </header>
-        <p className="App-intro">{this.state.apiResponse}</p>
-    </div>
-  );
+    );
+
+  }
 }
-}
-
-
-
-
 export default App;
